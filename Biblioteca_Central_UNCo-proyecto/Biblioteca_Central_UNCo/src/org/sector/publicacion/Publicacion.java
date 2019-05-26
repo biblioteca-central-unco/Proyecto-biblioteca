744941d9fa45990e0427c41237b4b99c38909164
GarGiN81 <49932858+GarGiN81@users.noreply.github.com>
2019-05-26 04:49:37 -0300

    subclase sector
    se completan con sus respectivos atributos, constructores y getters & setters, las subclases (publicacion y tesisCarrera) dependientes de la clase "Sector".

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/publicacion/Publicacion.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/publicacion/Publicacion.java
@@ -10,5 +10,38 @@ package org.sector.publicacion;
  * @author gichu
  */
 public class Publicacion {
+    private String artCientifico;
+    private String revista;
+    private String artAcademico;
+
+    public Publicacion(String artCientifico, String revista, String artAcademico) {
+        this.artCientifico = artCientifico;
+        this.revista = revista;
+        this.artAcademico = artAcademico;
+    }
+
+    public String getArtCientifico() {
+        return artCientifico;
+    }
+
+    public void setArtCientifico(String artCientifico) {
+        this.artCientifico = artCientifico;
+    }
+
+    public String getRevista() {
+        return revista;
+    }
+
+    public void setRevista(String revista) {
+        this.revista = revista;
+    }
+
+    public String getArtAcademico() {
+        return artAcademico;
+    }
+
+    public void setArtAcademico(String artAcademico) {
+        this.artAcademico = artAcademico;
+    }
     
 }
--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/tesisCarrera/TesisCarrera.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/tesisCarrera/TesisCarrera.java
@@ -10,5 +10,38 @@ package org.sector.tesisCarrera;
  * @author gichu
  */
 public class TesisCarrera {
+    private String tesisPosgrado;
+    private String tesisDoctorado;
+    private String tesisGrado;
+
+    public TesisCarrera(String tesisPosgrado, String tesisDoctorado, String tesisGrado) {
+        this.tesisPosgrado = tesisPosgrado;
+        this.tesisDoctorado = tesisDoctorado;
+        this.tesisGrado = tesisGrado;
+    }
+
+    public String getTesisPosgrado() {
+        return tesisPosgrado;
+    }
+
+    public void setTesisPosgrado(String tesisPosgrado) {
+        this.tesisPosgrado = tesisPosgrado;
+    }
+
+    public String getTesisDoctorado() {
+        return tesisDoctorado;
+    }
+
+    public void setTesisDoctorado(String tesisDoctorado) {
+        this.tesisDoctorado = tesisDoctorado;
+    }
+
+    public String getTesisGrado() {
+        return tesisGrado;
+    }
+
+    public void setTesisGrado(String tesisGrado) {
+        this.tesisGrado = tesisGrado;
+    }
     
 }
