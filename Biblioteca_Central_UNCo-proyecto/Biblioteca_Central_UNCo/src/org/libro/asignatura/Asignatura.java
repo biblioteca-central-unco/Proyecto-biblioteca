9da2b6fd187f1fae4719829a680a038797e1789b
GarGiN81 <49932858+GarGiN81@users.noreply.github.com>
2019-05-26 04:43:40 -0300

    clase asignatura
    Se colocan sus atributos, constructor y gets &sets

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/asignatura/Asignatura.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/asignatura/Asignatura.java
@@ -10,5 +10,26 @@ package org.libro.asignatura;
  * @author gichu
  */
 public class Asignatura {
-    
+    private String nombreAsign;
+    private int codigo;
+
+    public Asignatura(String nombreAsign, int codigo) {
+        this.nombreAsign = nombreAsign;
+        this.codigo = codigo;
+    }
+    public String getNombreAsign() {
+        return nombreAsign;
+    }
+
+    public void setNombreAsign(String nombreAsign) {
+        this.nombreAsign = nombreAsign;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/autor/Autor.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/autor/Autor.java
@@ -10,5 +10,59 @@ package org.libro.autor;
  * @author gichu
  */
 public class Autor {
+    private String nombre;
+    private String apellido;
+    private String nacionalidad;
+    private String asignaturaTema;
+    private int cantidadEjemplares;
+
+    public Autor(String nombre, String apellido, String nacionalidad, String asignaturaTema, int cantidadEjemplares) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacionalidad = nacionalidad;
+        this.asignaturaTema = asignaturaTema;
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getAsignaturaTema() {
+        return asignaturaTema;
+    }
+
+    public void setAsignaturaTema(String asignaturaTema) {
+        this.asignaturaTema = asignaturaTema;
+    }
+
+    public int getCantidadEjemplares() {
+        return cantidadEjemplares;
+    }
+
+    public void setCantidadEjemplares(int cantidadEjemplares) {
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
+    
     
 }
