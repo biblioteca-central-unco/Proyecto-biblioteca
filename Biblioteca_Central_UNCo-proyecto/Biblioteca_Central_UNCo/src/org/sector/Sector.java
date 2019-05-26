83c3aed83d1f2669affb6030e2369eb57cfad7d1
GarGiN81 <49932858+GarGiN81@users.noreply.github.com>
2019-05-26 04:52:58 -0300

    class Sector
    atributos, constructor y gets and sets de la clase sector.

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/Sector.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/sector/Sector.java
@@ -10,5 +10,38 @@ package org.sector;
  * @author gichu
  */
 public class Sector {
+    private String centralAsignatura;
+    private String patagonicaTesis;
+    private String posgradoPublicacion;
+
+    public Sector(String centralAsignatura, String patagonicaTesis, String posgradoPublicacion) {
+        this.centralAsignatura = centralAsignatura;
+        this.patagonicaTesis = patagonicaTesis;
+        this.posgradoPublicacion = posgradoPublicacion;
+    }
+
+    public String getCentralAsignatura() {
+        return centralAsignatura;
+    }
+
+    public void setCentralAsignatura(String centralAsignatura) {
+        this.centralAsignatura = centralAsignatura;
+    }
+
+    public String getPatagonicaTesis() {
+        return patagonicaTesis;
+    }
+
+    public void setPatagonicaTesis(String patagonicaTesis) {
+        this.patagonicaTesis = patagonicaTesis;
+    }
+
+    public String getPosgradoPublicacion() {
+        return posgradoPublicacion;
+    }
+
+    public void setPosgradoPublicacion(String posgradoPublicacion) {
+        this.posgradoPublicacion = posgradoPublicacion;
+    }
     
 }
