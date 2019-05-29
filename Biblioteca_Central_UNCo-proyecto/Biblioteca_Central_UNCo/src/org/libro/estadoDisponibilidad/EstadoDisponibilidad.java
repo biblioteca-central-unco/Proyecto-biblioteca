bcaed5b8dfd42580399c876abda38f948a6b7763
Natalia <noeminatalia96@gmail.com>
2019-05-29 16:39:18 -0300

    Clase estado disponibilidad
    agregue atributos, getter and setter y constructor

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/estadoDisponibilidad/EstadoDisponibilidad.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/libro/estadoDisponibilidad/EstadoDisponibilidad.java
@@ -10,5 +10,28 @@ package org.libro.estadoDisponibilidad;
  * @author gichu
  */
 public class EstadoDisponibilidad {
+    private Boolean disponibilidad;
+    private int cantidadEjemplares; 
+
+    public EstadoDisponibilidad(Boolean disponibilidad, int cantidadEjemplares) {
+        this.disponibilidad = disponibilidad;
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
+
+    public Boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public int getCantidadEjemplares() {
+        return cantidadEjemplares;
+    }
+
+    public void setCantidadEjemplares(int cantidadEjemplares) {
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
     
 }
