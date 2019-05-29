f2516fd080de259b333c4fdd657b2a0342eaf0db
Susana361 <49909398+Susana361@users.noreply.github.com>
2019-05-28 21:33:11 -0300

    En clase Persona
    Se saca el atributo cuil

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/biblioteca_central_unco/Biblioteca.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/biblioteca_central_unco/Biblioteca.java
@@ -10,5 +10,38 @@ package biblioteca_central_unco;
  * @author Susana
  */
 public class Biblioteca {
+    private String dirección;
+    private String pagWeb;
+    private int telefono;
+
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+
+    public String getPagWeb() {
+        return pagWeb;
+    }
+
+    public void setPagWeb(String pagWeb) {
+        this.pagWeb = pagWeb;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Biblioteca(String dirección, String pagWeb, int telefono) {
+        this.dirección = dirección;
+        this.pagWeb = pagWeb;
+        this.telefono = telefono;
+    }
     
 }
