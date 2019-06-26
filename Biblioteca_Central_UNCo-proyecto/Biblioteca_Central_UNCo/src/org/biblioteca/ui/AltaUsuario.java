fe97c6a5f0144746c535cc70143a82be45531716
Natalia <noeminatalia96@gmail.com>
2019-06-26 10:56:29 -0300

    cambios en Altausuario
    Elimino los botones cancelar y aceptar

--- Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/biblioteca/ui/AltaUsuario.java
+++ Biblioteca_Central_UNCo-proyecto/Biblioteca_Central_UNCo/src/org/biblioteca/ui/AltaUsuario.java
@@ -68,11 +68,6 @@ public class AltaUsuario extends javax.swing.JPanel {
         jlbl_contrasenia.setText("Contrasenia:");
 
         jbtn_aceptar.setText("Aceptar");
-        jbtn_aceptar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtn_aceptarActionPerformed(evt);
-            }
-        });
 
         jbtn_cancelar.setText("Cancelar");
         jbtn_cancelar.addActionListener(new java.awt.event.ActionListener() {
@@ -87,35 +82,36 @@ public class AltaUsuario extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jlbl_altausuario)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jlbl_nombreyapellido)
-                            .addComponent(jlbl_legajo)
-                            .addComponent(jlbl_dni)
-                            .addComponent(jlbl_telefono)
-                            .addComponent(jlbl_email)
-                            .addComponent(jlbl_direccion)
-                            .addComponent(jlbl_nombredeusuario)
-                            .addComponent(jlbl_contrasenia))
-                        .addGap(22, 22, 22)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jtxt_nombreyapellido)
-                            .addComponent(jtxt_legajo)
-                            .addComponent(jtxt_dni)
-                            .addComponent(jtxt_telefono)
-                            .addComponent(jtxt_email)
-                            .addComponent(jtxt_direccion)
-                            .addComponent(jtxt_nombredeusuario)
-                            .addComponent(jtxt_contrasenia, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(218, Short.MAX_VALUE)
-                .addComponent(jbtn_aceptar)
-                .addGap(18, 18, 18)
-                .addComponent(jbtn_cancelar)
-                .addGap(18, 18, 18))
+                            .addComponent(jlbl_altausuario)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jlbl_nombreyapellido)
+                                    .addComponent(jlbl_legajo)
+                                    .addComponent(jlbl_dni)
+                                    .addComponent(jlbl_telefono)
+                                    .addComponent(jlbl_email)
+                                    .addComponent(jlbl_direccion)
+                                    .addComponent(jlbl_nombredeusuario)
+                                    .addComponent(jlbl_contrasenia))
+                                .addGap(22, 22, 22)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jtxt_nombreyapellido)
+                                    .addComponent(jtxt_legajo)
+                                    .addComponent(jtxt_dni)
+                                    .addComponent(jtxt_telefono)
+                                    .addComponent(jtxt_email)
+                                    .addComponent(jtxt_direccion)
+                                    .addComponent(jtxt_nombredeusuario)
+                                    .addComponent(jtxt_contrasenia, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE))))
+                        .addContainerGap(36, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jbtn_aceptar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtn_cancelar)
+                        .addGap(20, 20, 20))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -154,24 +150,17 @@ public class AltaUsuario extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jlbl_contrasenia)
                     .addComponent(jtxt_contrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbtn_aceptar)
-                    .addComponent(jbtn_cancelar)))
+                    .addComponent(jbtn_cancelar))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbtn_cancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_cancelarActionPerformed
-      this.dispatchEvent(new WindowEvent (this, WindowEvent.WINDOW_CLOSING));
-        
+        dispose();
     }//GEN-LAST:event_jbtn_cancelarActionPerformed
-
-    private void jbtn_aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_aceptarActionPerformed
-       if(jtxt_contrasenia!=null & & jtxt_direccion!=null & & jtxt_dni!=null & & jtxt_email!=null & & jtxt_legajo!=null & & jtxt_nombredeusuario!=null & & jtxt_nombreyapellido!=null && jtxt_telefono!=null);
-        
-        System.out.println("no se puede guardar");
-    
-    }//GEN-LAST:event_jbtn_aceptarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -195,4 +184,8 @@ public class AltaUsuario extends javax.swing.JPanel {
     private javax.swing.JTextField jtxt_nombreyapellido;
     private javax.swing.JTextField jtxt_telefono;
     // End of variables declaration//GEN-END:variables
+
+    private void dispose() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
